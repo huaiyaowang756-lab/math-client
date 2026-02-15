e05584610cf9e639a8579ce520d92db5cf5961bd
huaiyaowang-cell <huaiyao.wang@bidderdesk.com>
2026-02-16 02:56:15 +0800

    feat: first save

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 24, fontFamily: 'sans-serif' }}>
+      <h1>Math Client</h1>
+      <p>React 前端项目已就绪</p>
+    </div>
+  )
+}
+
+export default App
